5158ac30ad35ee437d4d7b1639a12889bd60680b
de-cryptor <bim2014001@iiita.ac.in>
2020-12-15 13:30:28 +0530

    URL Model Added

--- project/nbapp/views.py
+++ project/nbapp/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 class Home(TemplateView):
     template_name = 'home.html'
     
 @csrf_exempt
 def shorturl(request):
+    if request.method == 'POST':
+        print(request.body)
+        print(request.POST)
     context_data = dict()
     context_data['short_url'] = "https://nbapp.com/gtyh567sd"
     return render(request,'shorturl_success.html',context_data)
+
+
--- project/project/settings.py
+++ project/project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'nbapp',
 ]
 
 SITE_ID = 1
