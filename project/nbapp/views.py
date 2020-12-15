741a67da4ca36e012443f9e5cd2e5d3f323f95d2
de-cryptor <bim2014001@iiita.ac.in>
2020-12-15 23:48:47 +0530

    404 Page Added

--- project/nbapp/views.py
+++ project/nbapp/views.py
@@ -10,46 +10,50 @@ from django.core import serializers
 import traceback
 
 class Home(TemplateView):
-    template_name = 'home.html'
+    try:
+        template_name = 'home.html'
+    except:
+        template_name = '404.html'
     
 @csrf_exempt
 def shorturl(request):
     context_data = dict()
-    short_url = ""
-    given_url = ""
-    if request.method == 'GET':
-        return render(request,'home.html',context_data)
-    
-    if request.method == 'POST':
-        data = request.POST 
-        given_url = data['entered_url']
-        base_url = "https://immense-island-94283.herokuapp.com/"
+    try:
+        short_url = ""
+        given_url = ""
+        if request.method == 'GET':
+            return render(request,'home.html',context_data)
         
-        if validators.url(given_url):
-            try:
-                url,created = URL.objects.get_or_create(
-                    full_url = data['entered_url'],
-                )
-                if not request.user.is_anonymous:
-                    url.created_by = request.user
-                url.save()
-                short_url = base_url + url.url_hash
-                
-            except:
-                print(traceback.format_exc(limit=None, chain=True))
+        if request.method == 'POST':
+            data = request.POST 
+            given_url = data['entered_url']
+            base_url = "https://immense-island-94283.herokuapp.com/"
+            
+            if validators.url(given_url):
+                try:
+                    url,created = URL.objects.get_or_create(
+                        full_url = data['entered_url'],
+                    )
+                    if not request.user.is_anonymous:
+                        url.created_by = request.user
+                    url.save()
+                    short_url = base_url + url.url_hash
+                    
+                except:
+                    print(traceback.format_exc(limit=None, chain=True))
+            else:
+                pass
+        if not request.user.is_anonymous:
+            created_urls = URL.objects.filter(created_by=request.user)
+            context_data['created_urls']  = json.loads(serializers.serialize("json",created_urls))
         else:
-            pass
-    if not request.user.is_anonymous:
-        print(request.user)
-        created_urls = URL.objects.filter(created_by=request.user)
-        context_data['created_urls']  = json.loads(serializers.serialize("json",created_urls))
-    else:
-        context_data['created_urls']  = []
-    context_data['short_url'] = short_url
-    context_data['given_url'] = given_url
-    context_data['base_url'] = base_url
-    print(context_data)
-    return render(request,'shorturl_success.html',context_data)
+            context_data['created_urls']  = []
+        context_data['short_url'] = short_url
+        context_data['given_url'] = given_url
+        context_data['base_url'] = base_url
+        return render(request,'shorturl_success.html',context_data)
+    except :
+        return render(request,'404.html',context_data)
 
 
 def get_client_ip(request):
@@ -73,3 +77,5 @@ def url_redirect(request,url_hash):
     )
     link.clicked()
     return HttpResponseRedirect(link.full_url)
+
+    
